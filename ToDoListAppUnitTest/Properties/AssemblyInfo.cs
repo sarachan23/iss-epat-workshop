8e9e1b599e622fd58f3b15d6b9923761af5f9ed2
sarachan23 <sarachan23@gmail.com>
2018-10-19 14:05:39 +0800

    add unit test for todolist

---
+++ ToDoListAppUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ToDoListAppUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ToDoListAppUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b508ad35-4fb1-43c9-bc4d-12cd8bed0411")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ToDoListAppUnitTest/ToDoListUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ToDoListAppUnitTest
+{
+    [TestClass]
+    public class ToDoListUnitTest
+    {
+        [TestInitialize()]
+        public void Initialize()
+        {
+            _list = new ToDoList();
+        }
+
+        [TestCleanup()]
+        public void Cleanup() { }
+
+        [TestMethod]
+        public void AddNewToDoItemTest()
+        {
+        }
+
+
+    }
+}
